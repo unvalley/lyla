3d4c74fe7ec4686f463d732517fc95fec3d447d9
unvalley <kirohi.code@gmail.com>
2022-07-06 01:22:30 +0900

    braking-changes: remove previous ml processing

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,21 +1,12 @@
 import numpy as np
-import transformers
 import os
-import torch
 import pandas as pd
 import concurrent.futures as confu
 from fastapi import FastAPI, Depends
 from pydantic import BaseModel
 from typing import Dict, Union, List, Any
-from torch.utils.data import Dataset, DataLoader
 from fastapi.middleware.cors import CORSMiddleware
-# --
-from app.example_generator import *
-from app.schema import *
-from app.score_regression import *
-from app.bert_dataset import *
-from app.usecase import *
-
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 app = FastAPI()
 
@@ -32,76 +23,50 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-score_regression_models = get_score_regression_models()
-example_generator_models = get_example_generator_models()
 
 
-@app.post("/predict/scores", response_model=List[ScoringResponse])
-async def predict_scores(request: AllScoringRequest):
+class TfidfRequest(BaseModel):
+    text: str
 
-    # TODO: パフォーマンス改善
-    text_df = pd.DataFrame({'answer': [request.text]})
-    BERT_MODEL = "cl-tohoku/bert-base-japanese-whole-word-masking"
-    tokenizer = transformers.BertJapaneseTokenizer.from_pretrained(
-        BERT_MODEL, output_attentions=True)
+class TfidfResponse(BaseModel):
+    text: str
 
-    with confu.ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
-        logicality_future = executor.submit(
-            score_regression_models["logicality"].predict, text_df)
-        validness_future = executor.submit(
-            score_regression_models["validness"].predict, text_df)
-        understanding_future = executor.submit(
-            score_regression_models["understanding"].predict, text_df)
-        writing_future = executor.submit(
-            score_regression_models["writing"].predict, text_df)
 
-        l_res = logicality_future.result()
-        v_res = validness_future.result()
-        u_res = understanding_future.result()
-        w_res = writing_future.result()
-
-        l_atten_handler = AttentionHandler(l_res["all_preds"], l_res["all_attentions"], next(
-            iter(l_res["data_loader"])), tokenizer)
-        v_atten_handler = AttentionHandler(v_res["all_preds"], v_res["all_attentions"], next(
-            iter(v_res["data_loader"])), tokenizer)
-        u_atten_handler = AttentionHandler(u_res["all_preds"], u_res["all_attentions"], next(
-            iter(u_res["data_loader"])), tokenizer)
-        w_atten_handler = AttentionHandler(w_res["all_preds"], w_res["all_attentions"], next(
-            iter(w_res["data_loader"])), tokenizer)
-
-        # attentionの追加
-        result = [
-            ScoringResponse(
-                measurement='論理性',
-                score=to_score(l_res["all_preds"]),
-                highlightIndex=l_atten_handler.calc_highlight_target_index()),
-            ScoringResponse(
-                measurement='妥当性',
-                score=to_score(v_res["all_preds"]),
-                highlightIndex=v_atten_handler.calc_highlight_target_index()),
-            ScoringResponse(
-                measurement='理解力',
-                score=to_score(u_res["all_preds"]),
-                highlightIndex=u_atten_handler.calc_highlight_target_index()),
-            ScoringResponse(
-                measurement='文章力',
-                score=to_score(w_res["all_preds"]),
-                highlightIndex=w_atten_handler.calc_highlight_target_index()),
+def get_tfidf(all_entities: list[str]):
+    """ TFIDFを計算し，その値を返す
+        all_enttiesは，spaceで分けた観点の文章のリスト
+        all_enttiites(文書群) = [
+            "部屋 スタッフ 景色",               <= ホテル1の全エンティティ
+            "城　ホテル　見晴らし　温泉",        <= ホテル2の全エンティティ
+            "トイレ　浴槽　駅　駐車場",          <= ホテル3の全エンティティ
+            ...
         ]
-        return result
+    """
+    # normとsmooothをオフにするには，norm=None, smooth_idf=Falseを加える
+    vectorizer = TfidfVectorizer(
+        token_pattern=u'(?u)\S+', lowercase=False, stop_words=None, max_df=0.5, min_df=0.05)
+    # 文書内の全単語のTF-IDF値を取得
+    tfidf = vectorizer.fit_transform(all_entities)
 
+    # 単語毎のTFIDF値配列: TF-IDF行列: [1つめの文書に対する各単語のベクトル値, ...]
+    tfidf_matrix = tfidf.toarray()
+    # index順の単語リスト
+    words = vectorizer.get_feature_names()
+    all_tfidfs = []
 
-@app.post("/predict/example_text", response_model=ExampleTextResponse)
-async def predict_example_text(request: ExampleTextRequest):
-    # 入力をどうするかとか考える必要あるけど，とりあえずtextを受け取ってtextのリストを返せればOK
-    # input: Attentionの重いテキストを入力とする
-    measurement = request.measurement
-    model = example_generator_models[measurement]
-    result = model.predict(request.text)
-    return {"exampleTexts": result}
+    for doc_idx, vec in zip(range(len(all_entities)), tfidf_matrix):
+        tfudfs = []
+        # 全文書にて，TFIDF値を単語順にソートして統一する
+        for word_idx, tfidf_value in sorted(enumerate(vec), key=lambda x: x[1], reverse=True):
+            word = words[word_idx]
+            tfudfs.append([word, float(tfidf_value)])
+        all_tfidfs.append(tfudfs)
+    return all_tfidfs
 
-
-def to_score(all_preds: list) -> float:
-    pred = pd.DataFrame(all_preds).transpose().mean(axis=1)
-    score = pred * 2 * 10
-    return score
+@app.post("/tfidf", response_model=TfidfResponse)
+async def calculate_tfidf(request: TfidfRequest):
+    text = request.text
+    # TODO: 観点リストに変換
+    tfidfs = get_tfidf([text])
+    print(tfidfs)
+    return 
